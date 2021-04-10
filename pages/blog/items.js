841b82cffe89feb46f3412f5b8f67a4af503e3ae
vince7844 <vincent.lasserre@efrei.net>
2021-04-10 14:40:48 +0300

    ajout de contenu sur la page items

--- pages/blog/items.js
+++ pages/blog/items.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { Layout } from '../../components/layout';
+import Head from 'next/head'; // Head like <head></head> in HTML
 
 const Items = () => (
-  <Layout>
-    <h1>Items</h1>
-  </Layout>
+  <> 
+    <Head>
+      <title>Items</title>
+    </Head>
+    <Layout>
+      <h1>Items</h1>
+      <ul>
+        <h2>Liste des catégories</h2>
+        <li>Culture</li>
+        <li>Géographie</li>
+        <li>Social</li>
+        <li>Economique</li>
+      </ul>
+    </Layout>
+  </>
 );
 
 export default Items;
